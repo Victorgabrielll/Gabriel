b781df537ed0d3071d4f121a162a183ae3aba7fe
Victorgabrielll <106012880+Victorgabrielll@users.noreply.github.com>
2022-05-22 07:53:40 -0300

    Update prova01_questao03.py

--- prova01_questao03.py
+++ prova01_questao03.py
@@ -2,6 +2,8 @@
 # e distinto x e y,calcular o mdc de x e y
 
 from math import gcd
+#biblioteca que auxilia na equação do MDC
 x = int(input('Digite o primeiro valor de x '))
 y = int(input('Digite o segundo valor de y '))
 print('O MDC(', x,',', y,') =',(gcd(x, y)))
+#resultado finaç
