e98512763fa066603d40c3733e966f3a96ae9aa3
Victorgabrielll <106012880+Victorgabrielll@users.noreply.github.com>
2022-05-22 07:33:36 -0300

    Create prova01_questao03.py

---
+++ prova01_questao03.py
@@ -0,0 +1,7 @@
+# Faça um programa,dado dois dados inteiros, positivos
+# e distinto x e y,calcular o mdc de x e y
+
+from math import gcd
+x = int(input('Digite o primeiro valor de x '))
+y = int(input('Digite o segundo valor de y '))
+print('O MDC(', x,',', y,') =',(gcd(x, y)))
